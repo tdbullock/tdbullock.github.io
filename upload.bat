96e0413b67fb052ea2cb987ffdd493dcbf17ac3c
Tom Bullock <tombullock@gmail.com>
2021-03-21 14:13:03 +0000

    Updated data

--- upload.bat
+++ upload.bat
@@ -1,3 +1,10 @@
-git add --all
+REM git add --all
+
+git add "C:\Toms\githubio\tdbullock.github.io\MSOA_Cases\msoa_casesJson.geojson"
+git add "C:\Toms\githubio\tdbullock.github.io\MSOA_Cases\updates.json
+
+git add "C:\Toms\githubio\tdbullock.github.io\Covid_UK_LA_Cases\LA_casesJson.geojson"
+git add "C:\Toms\githubio\tdbullock.github.io\Covid_UK_LA_Cases\updates.json
+
 git commit -m "Updated data"
 git push -u origin master
